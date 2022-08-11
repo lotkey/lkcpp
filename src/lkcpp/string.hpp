aeafec2a7d47ca4b073909e8c67cd7b52e2c3c19
Chris McVickar <mcvi1652@vandals.uidaho.edu>
2022-08-11 00:17:08 -0700

    Adding string

---
+++ src/lkcpp/string.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "lkcpp/vector.hpp"
+
+namespace lkcpp {
+class string : public vector<char> {
+public:
+private:
+};
+} // namespace lkcpp
--- src/test.cpp
+++
@@ -1,3 +0,0 @@
-#include "lkcpp/unique_ptr.hpp"
-
-int main() { auto ptr = lkcpp::make_unique<int>(); }
